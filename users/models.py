06756a4e8f4f651a7feecf5915613465fcb9a6d5
Maor323 <96150039+Maor323@users.noreply.github.com>
2023-05-22 23:47:03 +0300

    Customer-screen- maor (#15)
    * update
    
    * customers update
    
    * Delete users/__pycache__ directory
    
    ---------
    
    Co-authored-by: maorh <maorhylaptop@gmail.com>
    Co-authored-by: neta3200 <81813964+neta3200@users.noreply.github.com>

--- users/models.py
+++ users/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class UsersData(AbstractUser):
     resetCode = models.CharField(max_length=40, blank=True, default='', null=True)
-    lastPasswords = models.JSONField(blank=True, default='')
+    lastPasswords = models.JSONField(blank=True, default=dict)
     
   
  #     #Specify the required fields for user creation and management
